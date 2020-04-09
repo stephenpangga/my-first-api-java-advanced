9a64758a435a5878eb7007eef2d77cb3d1ca5f86
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-04-09 18:26:05 +0200

    Finished practice session, time to make my own code.

--- src/main/java/nl/inholland/myfirstapi/controller/GuitarController.java
+++ src/main/java/nl/inholland/myfirstapi/controller/GuitarController.java
@@ -1,4 +1,29 @@
 package nl.inholland.myfirstapi.controller;
 
+import nl.inholland.myfirstapi.model.Guitar;
+import nl.inholland.myfirstapi.service.GuitarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+//endpoint, also the request value /api/guitars
+@RequestMapping(value = "guitars")
+@CrossOrigin(origins = {"http://localhost:3000}", "http://localhost"})
 public class GuitarController {
+
+    @Autowired
+    private GuitarService service;
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Guitar>> getAllGuitars()
+    {
+        return ResponseEntity.status(404).body(service.getAllGuitars());
+    }
 }
